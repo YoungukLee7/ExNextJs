8334aeea2c2bf9d7067817d9db74eccb24849e44
YoungukLee7 <https://github.com/YoungukLee7/StartNextJs.git>
2024-05-07 15:08:14 +0900

    second commit

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,20 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default async function MovieDetail({
-    params: {id},
-    }:{
-        params: {id: string};
-    }) {
+// url에 있는 id 가져오기
+interface IParams {
+    params: {id: string};
+}
+
+export async function generateMetadata({params:{id}} : IParams) {
+    const movie = await getMovie(id);
+    return {
+        title: movie.title,
+    };
+}
+
+export default async function MovieDetailPage({params: {id}}: IParams) {
         return (
         <div>
             <h3>Movie detail</h3>
