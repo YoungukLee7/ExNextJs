03e59bbadc493e711fdab5b4665076d135b7205e
YoungukLee7 <https://github.com/YoungukLee7/StartNextJs.git>
2024-05-08 17:58:28 +0900

    seven commit

--- components/movie.tsx
+++ components/movie.tsx
@@ -14,6 +14,7 @@ export default function Movie({title, id, poster_path}: IMovieProps){
     
     // 아래 return하는 div 안에 Link가 제목에만 적용되어 이미지를 클릭해도 아무일도 발생하지 않는 상황인데
     // router를 사용하여 onClick 이벤트를 이용하면 이미지를 클릭해도 링크로 이동시킬 수 있다.
+    // useRouter는 클라이언트 사이드 라우팅을 처리하며 브라우저에서만 작동되기 때문에 "use client";를 붙여야 한다.
     const router = useRouter();
     const onclick = () => {
         router.push(`/movies/${id}`);
